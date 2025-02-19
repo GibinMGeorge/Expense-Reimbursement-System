55d365487bd3b88a36ed696f7c7c24f8cd24a30a
Gibin <gibinmgeorge2001@gmail.com>
2025-02-19 13:00:35 -0800

    created backend skeleton, db initialisation

--- P1Backend/src/main/java/com/revature/P1Backend/P1BackendApplication.java
+++ P1Backend/src/main/java/com/revature/P1Backend/P1BackendApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class P1BackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(P1BackendApplication.class, args);
+
+		System.out.println("ERS Backend Started");
 	}
 
 }

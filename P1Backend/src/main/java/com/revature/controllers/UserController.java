649b039dae9904524e86e3b00146d3943b56852e
Gibin <gibinmgeorge2001@gmail.com>
2025-02-19 21:02:04 -0800

    working on user controller

--- P1Backend/src/main/java/com/revature/controllers/UserController.java
+++ P1Backend/src/main/java/com/revature/controllers/UserController.java
@@ -1,4 +1,77 @@
 package com.revature.controllers;
 
+import com.revature.dto.UserDTO;
+import com.revature.models.User;
+import com.revature.service.ReimbursementService;
+import com.revature.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api/v1/users")
+
 public class UserController {
+    private final UserService userService;
+    private final ReimbursementService reimbursementService;
+
+    @Autowired
+    public UserController(UserService userService, ReimbursementService reimbursementService) {
+        this.userService = userService;
+        this.reimbursementService = reimbursementService;
+    }
+
+    // get all users, then return list of all users
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        List<UserDTO> users = userService.getAllUsers();
+
+        return ResponseEntity.ok(users);
+    }
+
+    // get a user by id and return that user
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long userId) {
+        UserDTO userDTO = userService.getUserById(userId);
+
+        return ResponseEntity.ok(userDTO);
+    }
+
+    // create a new user and return that user
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody User user) {
+        UserDTO createdUser = userService.createUser(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    // update a user role, user is selected by user id
+    @PatchMapping("/{userId}/role")
+    public ResponseEntity<UserDTO> updateUserRole(@PathVariable Long userId, @RequestBody Map<String, String> request) {
+        String newRole = request.get("role");
+        UserDTO updatedUser = userService.updateUserRole(userId, newRole);
+
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    // delete a user by their id
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    // get all reimbursements of a user by user id
+
+    // create a new reimbursement and return it
+
+    // update a reimbursement
+
+
 }

dfe3bc67910313b79cfa7f7c0b6f7238061babc0
Gibin <gibinmgeorge2001@gmail.com>
2025-02-20 11:46:38 -0800

    successfully connected DB

--- P1Backend/src/main/java/com/revature/P1Backend/P1BackendApplication.java
+++ P1Backend/src/main/java/com/revature/P1Backend/P1BackendApplication.java
@@ -2,8 +2,14 @@ package com.revature.P1Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.revature.models") //Tells spring to scan this package for DB entities
+@ComponentScan("com.revature") //Tells spring to scan this package for beans
+@EnableJpaRepositories("com.revature.repositories") //Enables JPA repositories in our DAOs package
 public class P1BackendApplication {
 
 	public static void main(String[] args) {
--- P1Backend/src/main/java/com/revature/repositories/ReimbursementRepository.java
+++ P1Backend/src/main/java/com/revature/repositories/ReimbursementRepository.java
@@ -1,11 +1,15 @@
 package com.revature.repositories;
 
 import com.revature.models.Reimbursement;
+import com.revature.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ReimbursementRepository {
+@Repository
+public interface ReimbursementRepository extends JpaRepository<Reimbursement, Long> {
 
     // find reimbursements by status
     List<Reimbursement> findByStatus(Reimbursement.Status status);

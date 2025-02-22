6201773ed0c65f28c4f2c7d81673615cb20e34bd
Gibin <gibinmgeorge2001@gmail.com>
2025-02-22 11:44:03 -0800

    updated backend

--- P1Backend/src/main/java/com/revature/controllers/ReimbursementController.java
+++ P1Backend/src/main/java/com/revature/controllers/ReimbursementController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/reimbursements")
+@RequestMapping("/api/gmg/reimbursements")
 @CrossOrigin
 
 public class ReimbursementController {
--- P1Backend/src/main/java/com/revature/controllers/UserController.java
+++ P1Backend/src/main/java/com/revature/controllers/UserController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/gmg/users")
 @CrossOrigin
 
 public class UserController {
